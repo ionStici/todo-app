72908171e6d0f58a5c9082ecf65f0c4ad01ad644
Ion Stici <ionsticidev@gmail.com>
2022-11-24 10:43:46 +0200

    Update JS

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
 
-class App {}
+class App {
+    uncheckedContainer = document.querySelector(".app__unchecked");
+    checkedContainer = document.querySelector(".app__checked");
+
+    form = document.querySelector(".app__input__form");
+    input = document.querySelector(".app__input__text-box");
+    button = document.querySelector(".app__input__button");
+
+    #uncheckedTodos = [];
+    #checkedTodos = [];
+
+    #html = "";
+    #html2 = "";
+
+    constructor() {}
+}
 
 const app = new App();
